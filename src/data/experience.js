78b2e77e88a3a29ab80c3839dcd91c55c701aeba
chrismarquezz <chrismarquez00@icloud.com>
2025-06-07 13:25:49 -0400

    Changed tense for experience

--- src/data/experience.js
+++ src/data/experience.js
@@ -7,9 +7,9 @@ export const experienceData = [
     company: "ClassLink",
     date: "June 2025 - Present",
     description: [
-      "Architecting a system to ingest and process roster data for millions of student and teacher accounts using RESTful APIs, and implementing a secure SSO login flow with OAuth2/OIDC",    
-      "Building a responsive and intuitive user interface from the ground up with React and Tailwind CSS, all supported by a scalable AWS-based backend",
-      "Creating comprehensive API reference documentation and setup guides to streamline future partner integrations and ensure the project's long-term success",
+      "Architect a system to ingest and process roster data for millions of student and teacher accounts using RESTful APIs, and implementing a secure SSO login flow with OAuth2/OIDC",    
+      "Build a responsive and intuitive user interface from the ground up with React and Tailwind CSS, all supported by a scalable AWS-based backend",
+      "Create comprehensive API reference documentation and setup guides to streamline future partner integrations and ensure the project's long-term success",
     ],
     type: "Work",
     institutionLogo: ClassLinkLogo, 
@@ -31,7 +31,7 @@ export const experienceData = [
     date: "August 2022 - May 2026 (Expected)",
     description: [
       "Relevant Coursework: Software Engineering, Data Structures & Algorithms, Principles of Database Systems",
-      "Focusing on developing a strong foundation in computer science principles and practical software development",
+      "Focuse on developing a strong foundation in computer science principles and practical software development",
     ],
     type: "Education",
     institutionLogo: VillanovaLogo,
